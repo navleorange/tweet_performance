e8e6858c89454ec02bee4d74665f7893c3afaf7e
navleorange <navleorange.0718@gmail.com>
2022-08-19 07:08:36 +0900

    fix:chrome-driver

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import libs
 import os
 from dotenv import load_dotenv
 import psycopg2
-import chromedriver_binary
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
